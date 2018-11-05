222ebfd55e39a171d6ca65f67e5cd7c2f1772b38
Mihajlo <mihajlosmex@gmail.com>
2018-11-05 10:59:37 +0100

    template update

--- server/server.js
+++ server/server.js
@@ -1,14 +1,46 @@
 const path = require('path');
+const http = require('http');
+const moment = require('moment');
 const express = require('express');
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
 
+io.on("connect", function (socket) {
+    console.log("New user connected !");
+
+     socket.broadcast.emit("messages", {
+         name: "Admin",
+         text: "Hello to everyone !"
+     })
+
+    socket.on("newMess", function (mess) {
+        console.log(`New message ${mess}`);
+        
+        io.emit("messages", {
+            name:mess.name,
+            text:mess.text,
+            timeStamp:moment(mess.timeStamp).format('lll'),
+        });
+
+
+    })
 
 
 
-app.listen(port, function(){
+    socket.on("disconnect", function () {
+        console.log("User was disconnected");
+    });
+
+});
+
+
+
+server.listen(port, function () {
     console.log(`Server is up on ${port}`);
 });
